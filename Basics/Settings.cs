af453d0b1b884b9bd817ce30e1debc6194ef3dd0
Patrick Hudson <phudson2@gmail.com>
2013-10-19 21:52:31 -0500

    Copyright

--- Basics/Settings.cs
+++ Basics/Settings.cs
@@ -7,6 +7,8 @@
 * Universal Chevereto Uploadr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 * You should have received a copy of the GNU General Public License along with Universal Chevereto Uploadr. If not, see http://www.gnu.org/licenses/.
+ * test
+ * 
 */
 
 using System;
